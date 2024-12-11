88421d5cbe19e80387a5a70c397eb01de4cb01af
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-12-11 16:21:14 +0200

    Nodemon with TypeScript

--- src/app.ts
+++ src/app.ts
@@ -3,4 +3,6 @@ import todo from "./routes/todo";
 const app = express();
 app.use(express.json());
 app.use("/api", todo);
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("listening to port 3000");
+});
